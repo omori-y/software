5207c26bc154b8ff7d91a44b415bddf4c7a4b610
omori-y <omoriy04192004@gmail.com>
2025-07-04 15:25:50 +0900

    Create OthelloBoard.java

---
+++ src/main/java/org/example/OthelloBoard.java
@@ -0,0 +1,80 @@
+public class OthelloBoard {
+    public int[][] board = new int[8][8];
+    private static final int BOARD_SIZE = 8;
+    private static final int[] DX = {-1, -1, -1, 0, 0, 1, 1, 1};
+    private static final int[] DY = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    public OthelloBoard() {
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int r = 0; r < BOARD_SIZE; r++)
+            for (int c = 0; c < BOARD_SIZE; c++)
+                board[r][c] = 0;
+        board[3][3] = 2; board[3][4] = 1;
+        board[4][3] = 1; board[4][4] = 2;
+    }
+
+    public boolean canPlace(int row, int col, int player) {
+        if (board[row][col] != 0) return false;
+        int opponent = (player == 1) ? 2 : 1;
+
+        for (int d = 0; d < 8; d++) {
+            int r = row + DX[d];
+            int c = col + DY[d];
+            boolean hasOpponentBetween = false;
+            while (r >= 0 && r < BOARD_SIZE && c >= 0 && c < BOARD_SIZE) {
+                if (board[r][c] == opponent) {
+                    hasOpponentBetween = true;
+                } else if (board[r][c] == player) {
+                    if (hasOpponentBetween) return true;
+                    else break;
+                } else {
+                    break;
+                }
+                r += DX[d]; c += DY[d];
+            }
+        }
+        return false;
+    }
+
+    public void flip(int row, int col, int player) {
+        int opponent = (player == 1) ? 2 : 1;
+        board[row][col] = player;
+
+        for (int d = 0; d < 8; d++) {
+            int r = row + DX[d];
+            int c = col + DY[d];
+            boolean hasOpponentBetween = false;
+            while (r >= 0 && r < BOARD_SIZE && c >= 0 && c < BOARD_SIZE) {
+                if (board[r][c] == opponent) {
+                    hasOpponentBetween = true;
+                } else if (board[r][c] == player) {
+                    if (hasOpponentBetween) {
+                        int flipR = row + DX[d];
+                        int flipC = col + DY[d];
+                        while (flipR != r || flipC != c) {
+                            board[flipR][flipC] = player;
+                            flipR += DX[d];
+                            flipC += DY[d];
+                        }
+                    }
+                    break;
+                } else {
+                    break;
+                }
+                r += DX[d]; c += DY[d];
+            }
+        }
+    }
+
+    public boolean hasAnyValidMove(int player) {
+        for (int r = 0; r < BOARD_SIZE; r++) {
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                if (canPlace(r, c, player)) return true;
+            }
+        }
+        return false;
+    }
+}
